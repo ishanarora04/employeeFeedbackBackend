9f5480289fc80af070f356f89f26eaf161ec9f92
Ishan Arora <ishanarora04@gmail.com>
2020-07-25 17:51:38 +0530

    Added changes

--- routes/admin/adminController.js
+++ routes/admin/adminController.js
@@ -1,12 +1,12 @@
-
 class AdminController {
   constructor(adminService) {
     this.adminService = adminService;
   }
 
   async getEmployeesToAssignForFeedback(req, res) {
-    
-
-
+    const employees = await this.adminService.getEmployeesToAssignForFeedback(
+      req.params
+    );
+    return employees;
   }
 }
